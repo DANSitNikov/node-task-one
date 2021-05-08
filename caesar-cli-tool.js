ba793c74454b808ccfd6c5a33f34fd99aff4f9e6
Daniil <sitnikovdan13@gmail.com>
2021-05-08 15:07:27 +0300

    refactor: delete everything from main branch

--- caesar-cli-tool.js
+++
@@ -1,8 +0,0 @@
-const { checkFiles } = require('./modules/check-files');
-const { checkNecessaryParameters } = require('./modules/necessary-parameters');
-const { getShift, getAction } = require('./modules/options');
-
-checkNecessaryParameters('shift', getShift());
-checkNecessaryParameters('action', getAction());
-
-checkFiles();

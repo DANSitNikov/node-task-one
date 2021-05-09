c8ea93616da6cf02b0e128cb55110835af12dc44
Daniil <sitnikovdan13@gmail.com>
2021-05-09 15:09:35 +0300

    fix: fix options

--- modules/options.js
+++ modules/options.js
@@ -3,8 +3,8 @@ const { program } = require('commander');
 program.version('1.0.0');
 
 program
-  .option('-s, --shift [type]')
-  .option('-i, --input [type]')
+  .option('-s, --shift <type>')
+  .option('-i, --input <type>')
   .option('-o, --output [type]')
   .option('-a, --action [type]')
   .parse();
